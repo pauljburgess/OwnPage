7eed903ae57920473a32dd2b87a3a32e53c7bc6a
Paul Burgess <paul@burgess.io>
2024-08-14 11:48:15 -0400

    Create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route, NavLink } from 'react-router-dom'
 import './App.css'
 import Nav from './ components/Nav'
+import Home from './ components/Home'
+import About from './ components/About'
+import DataScience from './ components/DataScience'
+import WebDev from './ components/WebDev'
 
 function App() {
  
@@ -11,7 +14,14 @@ function App() {
 
       <Nav />
       <h1>Paul Burgess</h1>
-
+      <main>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/WebDev" element={<WebDev/>}/>
+          <Route path="/DataScience" element={<DataScience/>}/>
+        </Routes>
+      </main>
 
     </div>
   )
