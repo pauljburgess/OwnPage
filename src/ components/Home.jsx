ba63a8eb3c84a0f037d20add58a12c2a7f913a9c
Paul Burgess <paul@burgess.io>
2024-09-18 14:26:28 -0400

    Add avatar

--- src/ components/Home.jsx
+++ src/ components/Home.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='content'> Home </div>
+    <div className='content'> 
+      <img id="avatar" src="../../photos/avatar.png" alt="" />
+    
+    </div>
   )
 }
 
