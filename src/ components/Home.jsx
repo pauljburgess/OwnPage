292969d7377a1d89137440f2ff1036c988da85e4
Paul Burgess <paul@burgess.io>
2024-09-18 15:51:45 -0400

    Style landing page

--- src/ components/Home.jsx
+++ src/ components/Home.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='content'> 
-      <h2>Hi there!</h2>
-      <p id='intro' >My name is Paul, welcome to my online business card/CV/resume. Take a gander through the links on the side to learn more about me and the projects that I've been involved in.</p>
-      <img id="avatar" src="../../photos/avatar.png" alt="" />
-    
+    <div className='content'>
+        <img id="avatar" src="../../photos/avatar.png" alt="" />
+        <h2>Hi there!</h2>
+        <p id='intro' >
+          My name is Paul, welcome to my online business card/CV/resume. Take a gander through the links on the side to learn more about me and the projects that I've been involved in.
+        </p>
     </div>
   )
 }
