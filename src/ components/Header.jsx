a21e246bdaf2bab74d1e703c4e0267582408de20
Paul Burgess <paul@burgess.io>
2024-09-18 16:29:02 -0400

    Add inner-banner to the header, adjust styling

--- src/ components/Header.jsx
+++ src/ components/Header.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Header = () => {
   return (
     <div className="header">
-      <h1 id="nametag">Paul Burgess</h1>
+      <div id='inner-banner'>
+        <h1 id="nametag">Paul Burgess</h1>
+      </div>
     </div>
 
   )
